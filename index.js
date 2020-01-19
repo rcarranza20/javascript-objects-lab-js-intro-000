ee0f2a52eaa42803de1af563469044e852aab885
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-19 23:13:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = new Object()
 
 function updateObjectWithKeyAndValue(object, key, value){
- return Object.assign({},{key:'value'})
+ return Object.assign({}, object,{key:'value'})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
