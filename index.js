dad7d07838dd5d5b4db998851a0444fb44a27d6a
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-19 23:09:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-function 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object['key']='value'
+  return object
+}
